b5fc9c69cc9a3e019639166df4981b90c1d7b86e
Bruno Araujo <tavaresaraujo.b@gmail.com>
2020-04-18 22:38:53 -0300

    Config js and css

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,8 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+
+//= link jquery-2.2.4.min.js
+//= link popper.min.js
+//= link bootstrap.min.js
+//= link plugins.js
+//= link active.js
