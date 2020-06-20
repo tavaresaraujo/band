0bca2913b2d328828579ccd05182925c8371b2eb
Bruno Araujo <you@example.comtavaresaraujo.b@gmail.com>
2020-06-20 20:11:20 -0300

    implements events page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'events/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
   resources :biography
+  resources :events
 end
