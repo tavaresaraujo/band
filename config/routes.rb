ecb96ce7a4b49df7cc333b95e8167efb5663a5e1
Bruno Araujo <you@example.comtavaresaraujo.b@gmail.com>
2020-06-21 12:36:17 -0300

    Implements contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'contact/index'
   get 'events/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
   resources :biography
   resources :events
+  resources :contact
 end
