8312f7391b46b35134cc6f6adc71de0c585d00ef
Bruno Araujo <tavaresaraujo.b@gmail.com>
2020-07-19 14:22:34 -0300

    static assets true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  config.serve_static_assets = true
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
